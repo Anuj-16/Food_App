a7599ff275ee7105731706ad0df3f56d5a672e2c
Sanjeev Kushwaha <sanjeevk249116@gmail.com>
2023-07-05 19:27:24 +0530

    done data

--- cart.js
+++ cart.js
@@ -1,37 +1,152 @@
+let item1=document.getElementById("items1");
+let priceItems=document.getElementById("priceItems");
+let cart1 = JSON.parse(localStorage.getItem("cart1")) || [];
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-if (cart.length == 0) {
-  console.log("empty");
-
-  let main = document.getElementById("main1");
-  let div1 = document.createElement("div");
-  div1.setAttribute("class", "main11");
-  let h1 = document.createElement("h1");
-  h1.innerHTML = " Your Food Cart is empty";
-  let p = document.createElement("p");
-  p.innerHTML =
-    "Your shopping cart is waiting. Give it purpose – fill it with Salad, Sandwich, Bread, Steak, Tuna Steak, Fish, Shrimp, Rice and more.";
-  //    let br=document.createElement("br");
-  //    br.innerHTML=br;
-  let p1 = document.createElement("p");
-  p1.innerHTML =
-    " Continue shopping on the home page , learn about today's deals ,or visit your wish list.";
-
-  div1.append(h1, p, p1);
-  main.append(div1);
-} else if (cart.length != 0) {
-  console.log("not empty");
-}
-
-detailMain22();
-
-async function detailMain22() {
-  let result = await fetch(`  http://localhost:3000/cartData`);
-  let data = await result.json();
+showData();
+async function showData(){
+  let result=await fetch("http://localhost:3000/Data");
+  let data=await result.json();
   console.log(data);
-  showDetails22(data);
+  cart1=data;
+  item1.innerHTML=data.length;
+  localStorage.setItem("cart1",JSON.stringify(data));
+  priceData(data);
+  Appendata(data);
+  
+}
+// console.log(data);
+
+ console.log(cart)
+//  cartData1(cart);
+//  function cartData1(cart){
+//      PostData(cart)
+//   // cart.forEach((el)=>{
+
+//   // })
+//  }
+function Appendata(data){
+  //item1.innerText=data.length;
+  if (data.length==0) {
+    console.log("empty");
+  
+    let main = document.getElementById("main1");
+    let div1 = document.createElement("div");
+    div1.setAttribute("class", "main11");
+    let h1 = document.createElement("h1");
+    h1.innerHTML = " Your Food Cart is empty";
+    let p = document.createElement("p");
+    p.innerHTML =
+      "Your shopping cart is waiting. Give it purpose – fill it with Salad, Sandwich, Bread, Steak, Tuna Steak, Fish, Shrimp, Rice and more.";
+    //    let br=document.createElement("br");
+    //    br.innerHTML=br;
+    let p1 = document.createElement("p");
+    p1.innerHTML =
+      " Continue shopping on the home page , learn about today's deals ,or visit your wish list.";
+  
+    div1.append(h1, p, p1);
+    main.append(div1);
+  } 
+  else if (data.length != 0) {
+    console.log("hii3");
+    let main1 = document.getElementById("main1");
+    data.forEach((el,idx) => {
+      let div = document.createElement("div");
+      div.setAttribute("id", "main11");
+  
+      let div1 = document.createElement("div");
+      div1.setAttribute("id", "main111");
+      let img = document.createElement("img");
+      img.src = el.img;
+      div1.append(img);
+  
+      let div2 = document.createElement("div");
+  
+      let strMeal = document.createElement("h2");
+      strMeal.innerHTML = el.title;
+      let desc=document.createElement("p")
+      desc.innerHTML=el.description;
+  
+      let price = document.createElement("h4");
+      price.setAttribute("id","priceNum")
+      price.innerHTML = "₹"+el.price;
+  let div3=document.createElement("div")
+  
+  let btn1 = document.createElement("button");
+  btn1.innerHTML = "+";
+  btn1.setAttribute("class", "plus");
+  btn1.addEventListener("click",()=>{
+    priceData1(el);
+  })
+  let btn2 = document.createElement("button");
+  btn2.innerHTML = "-";
+  btn2.addEventListener("click",()=>{
+    priceDataDelete(el);
+  })
+  btn2.setAttribute("class", "plus");
+  let btn3 = document.createElement("button");
+  btn3.innerHTML = "Delete";
+  btn3.addEventListener("click",()=>{
+   
+    deleteData(el.id)
+    console.log(el.id)
+
+  })
+  let btn4=document.createElement("button");
+  btn4.setAttribute("class", "plus");
+    btn4.setAttribute("id","btn44");
+    btn4.innerHTML="Buy"
+    btn4.addEventListener("click",()=>{
+      
+    })
+  
+  btn3.setAttribute("class", "plus");
+  
+  
+  div3.append(btn1,btn2,btn3,btn4)
+     
+      div2.append(strMeal,desc, price, div3);
+      div.append(div1, div2);
+  
+      main1.append(div);
+    });
+  }
 }
 
+let cartData =[
+            
+  {
+      title: "Baingan Bharta",
+      img: "https://www.themealdb.com/images/media/meals/urtpqw1487341253.jpg",
+      idMeal: "52807",
+      price:750,
+      description:"Baingan bharta is a popular north Indian dish where fire roasted eggplant is mashed and cooked with spices."
+    },
+    {
+      title: "Chicken Handi",
+      img: "https://www.themealdb.com/images/media/meals/wyxwsp1486979827.jpg",
+      idMeal: "52795",
+      price:899,
+      description:" Handi chicken is a delicious dish made with chicken cooked on a low flame for a long time."
+    },
+    {
+      title: "Dal fry",
+      img: "https://www.themealdb.com/images/media/meals/wuxrtu1483564410.jpg",
+      idMeal: "52785",
+      price:670,
+      description:"Dal Fry is a delicious and popular Indian lentil recipe that is made with tuvar dal (pigeon pea lentils), onions, tomatoes and spices."
+    },
+    {
+      title: "Kidney Bean Curry",
+      img: "https://www.themealdb.com/images/media/meals/sywrsu1511463066.jpg",
+      idMeal: "52868",
+      price:234,
+      description:"This delicious vegetarian stew is made in the pressure cooker, using dried kidney beans and a spice paste with garlic, ginger, and onion."
+    }
+  ]
+ console.log(cartData);
+  showDetails22(cartData);
+ 
 function showDetails22(data) {
   let main221 = document.getElementById("main221");
   main221.innerHTML = null;
@@ -42,20 +157,23 @@ function showDetails22(data) {
     let div1 = document.createElement("div");
     div1.setAttribute("id", "main2211");
     let img = document.createElement("img");
-    img.src = el.strMealThumb;
+    img.src = el.img;
     div1.append(img);
 
     let div2 = document.createElement("div");
 
     let strMeal = document.createElement("p");
-    strMeal.innerHTML = el.strMeal;
+    strMeal.innerHTML = el.title;
 
     let price = document.createElement("p");
-    price.innerHTML = el.cost;
+    price.innerHTML = "₹"+el.price;
 
     let btn = document.createElement("button");
     btn.innerHTML = "Add to cart";
     btn.setAttribute("id", "btnMain2");
+    btn.addEventListener("click", () => {
+      mystoreData(el);
+    });
 
     div2.append(strMeal, price, btn);
     div.append(div1, div2);
@@ -63,3 +181,109 @@ function showDetails22(data) {
     main221.append(div);
   });
 }
+
+ async function mystoreData(el) {
+  
+  let storeProduct = cart1.filter((elm) => {
+    return elm.img == el.img;
+  });
+
+  if (storeProduct.length > 0) {
+    alert("This product already in cart.");
+  } else {
+    try{
+      let new_obj = {
+        img: el.img,
+        title: el.title,
+        description:el.description,
+        price: el.price,
+      };
+      
+      let response = await fetch("http://localhost:3000/Data", {
+        method: "POST",
+        body: JSON.stringify(new_obj),
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+    
+      let data = await response.json();
+      console.log(data);
+    }catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+// async function PostData(el){
+
+//   try{
+//     let new_obj = {
+//       img: el.img,
+//       title: el.title,
+//       description:el.description,
+//       price: el.price,
+//     };
+    
+//     let response = await fetch("http://localhost:3000/Data", {
+//       method: "POST",
+//       body: JSON.stringify(new_obj),
+//       headers: {
+//         "Content-type": "application/json",
+//       },
+//     });
+  
+//     let data = await response.json();
+//     console.log(data);
+//   }catch (error) {
+//     console.log(error);
+//   }
+// }
+
+async function deleteData(id,idx){
+
+  let dataId=id;
+    try{
+  
+      let result=await fetch(`http://localhost:3000/Data/${dataId}`,{
+        method:"DELETE",
+        headers:{
+          "Content-type": "application/json"
+        },
+      })
+      let data=await result.json();
+      console.log(data);
+  
+  
+    }catch(error){
+      console.log(error);
+    }
+  
+  
+  }
+  
+function priceData(data){
+  console.log(data)
+  data.forEach((el)=>{
+   console.log(typeof el.price)
+    priceItems.innerHTML=+(priceItems.innerHTML)+el.price;
+ })
+
+}
+//let newPrice=document.getElementById("priceNum")
+//console.log(newPrice.innerHTML)
+function priceData1(el){
+  priceItems.innerHTML=+(priceItems.innerHTML)+el.price;
+  // price.innerHTML=+(priceItems.innerHTML)+el.price;
+  
+}
+
+function priceDataDelete(el){
+  if(priceItems.innerHTML>=el.price){
+    priceItems.innerHTML=+(priceItems.innerHTML)-el.price;
+  }
+ 
+  
+}
+
+
---
+++ cart2.js
@@ -0,0 +1,102 @@
+console.log("dfgh")
+
+let dataObj=[
+    {
+      title:"Chocolate Gateau",
+      price:345,
+      description:"Preheat the oven to 180°C/350°F/Gas Mark 4. Grease and line the base of an 8 in round spring form cake tin with baking parchment\r\nBreak the chocolate into a heatproof bowl and place over a saucepan of gently simmering water and stir until it melts.",
+      img:"https://www.themealdb.com/images/media/meals/tqtywx1468317395.jpg",
+    },
+    {
+      title:"Chicken Enchilada Casserole",
+      price:465,
+      description:"Cut each chicken breast in about 3 pieces, so that it cooks faster and put it in a small pot. Pour Enchilada sauce over it and cook covered on low to medium heat until chicken is cooked through, about 20 minutes.",
+      img:"https://www.themealdb.com/images/media/meals/qtuwxu1468233098.jpg",
+    },
+    {
+      title:"Cream Cheese Tart",
+      price:245,
+      description:"Crust: make a dough from 250g flour (I like mixing different flours like plain and wholegrain spelt flour), 125g butter, 1 egg and a pinch of salt, press it into a tart form and place it in the fridge.",
+      img:"https://www.themealdb.com/images/media/meals/wurrux1468416624.jpg",
+    },
+    {
+      title:"Christmas Pudding Flapjack",
+      price:645,
+      description:"Preheat the oven to 180°C/fan 160°C/gas mark 4 and grease and line a 25cm x 20cm tin. Melt the butter, sugar, syrup and orange zest in a large saucepan over a medium heat.",
+      img:"https://www.themealdb.com/images/media/meals/vvusxs1483907034.jpg",
+    },
+    {
+      title:"Chicken Handi",
+      price:890,
+      description:"Take a large pot or wok, big enough to cook all the chicken, and heat the oil in it. Once the oil is hot, add sliced onion and fry them until deep golden brown. Then take them out on a plate and set aside.\r\nTo the same pot, add the chopped garlic and sauté for a minute.",
+      img:"https://www.themealdb.com/images/media/meals/wyxwsp1486979827.jpg",
+    },
+    {
+      title:"Chicken Alfredo Primavera",
+      price:805,
+      description:"Heat 1 tablespoon of butter and 2 tablespoons of olive oil in a large skillet over medium-high heat. Season both sides of each chicken breast with seasoned salt and a pinch of pepper. Add the chicken to the skillet and cook for 5-7 minutes on each side, or until cooked through.",
+      img:"https://www.themealdb.com/images/media/meals/syqypv1486981727.jpg",
+    },
+    {
+      title:"Cajun spiced fish tacos",
+      price:799,
+      description:"Cooking in a cajun spice and cayenne pepper marinade makes this fish super succulent and flavoursome. Top with a zesty dressing and serve in a tortilla for a quick, fuss-free main that's delightfully summery.",
+      img:"https://www.themealdb.com/images/media/meals/uvuyxu1503067369.jpg",
+    },
+    {
+      title:"Crock Pot Chicken Baked Tacos",
+      price:499,
+      description:"Put the uncooked chicken breasts in the crock pot. Pour the full bottle of salad dressing over the chicken. Sprinkle the rest of the ingredients over the top and mix them in a bit with a spoon.\r\nCover your crock pot with the lid and cook on high for 4 hours.",
+      img: "https://www.themealdb.com/images/media/meals/ypxvwv1505333929.jpg",
+    },
+    {
+      title:"Chilli prawn linguine",
+      price:345,
+      description:"Mix the dressing ingredients in a small bowl and season with salt and pepper. Set aside.\r\n\r\nCook the pasta according to the packet instructions. Add the sugar snap peas for the last minute or so of cooking time.",
+      img:"https://www.themealdb.com/images/media/meals/usywpp1511189717.jpg",
+    },
+    {
+      title:"Clam chowder",
+      price:245,
+      description:"Rinse the clams in several changes of cold water and drain well. Tip the clams into a large pan with 500ml of water. Cover, bring to the boil and simmer for 2 mins until the clams have just opened. Tip the contents of the pan into a colander over a bowl to catch the clam stock.",
+      img: "https://www.themealdb.com/images/media/meals/rvtvuw1511190488.jpg",
+    },
+    {
+      title:"Creamy Tomato Soup",
+      price:245,
+      description:"Put the oil, onions, celery, carrots, potatoes and bay leaves in a big casserole dish, or two saucepans. Fry gently until the onions are softened – about 10-15 mins. Fill the kettle and boil it.\r\nStir in the tomato purée, sugar, vinegar, chopped tomatoes and passata, then crumble in the stock cubes.",
+      img: "https://www.themealdb.com/images/media/meals/stpuws1511191310.jpg",
+    },
+    {
+      title:"Chicken & mushroom Hotpot",
+      price:782,
+      description:"Heat oven to 200C/180C fan/gas 6. Put the butter in a medium-size saucepan and place over a medium heat. Add the onion and leave to cook for 5 mins, stirring occasionally. Add the mushrooms to the saucepan with the onions.",
+      img:"https://www.themealdb.com/images/media/meals/uuuspp1511297945.jpg",
+    },
+    {
+      title:"Chicken Couscous",
+      price:705,
+      description:"Heat the olive oil in a large frying pan and cook the onion for 1-2 mins just until softened. Add the chicken and fry for 7-10 mins until cooked through and the onions have turned golden. Grate over the ginger, stir through the harissa to coat everything and cook for 1 min more.",
+      img: "https://www.themealdb.com/images/media/meals/qxytrx1511304021.jpg",
+    },
+    {
+      title:"Chocolate Avocado Mousse",
+      price:200,
+      description:" Blend all the mousse ingredients together in your food processor until smooth. Add the cacao powder first and, as you blend, have all the ingredients to hand in order to adjust the ratios slightly as the size of avocados and bananas varies so much.",
+      img: "https://www.themealdb.com/images/media/meals/uttuxy1511382180.jpg",
+    },
+    {
+      title:"Choc Chip Pecan Pie",
+      price:249,
+      description:"First, make the pastry. Tip the ingredients into a food processor with 1 /4 tsp salt. Blend until the mixture resembles breadcrumbs. Drizzle 2-3 tsp cold water into the funnel while the blade is running – the mixture should start to clump together.",
+      img:"https://www.themealdb.com/images/media/meals/rqvwxt1511384809.jpg",
+    },
+    {
+      title:"Carrot Cake",
+      price:199,
+      description:"For the carrot cake, preheat the oven to 160C/325F/Gas 3. Grease and line a 26cm/10in springform cake tin.\r\nMix all of the ingredients for the carrot cake, except the carrots and walnuts, together in a bowl until well combined.",
+      img:"https://www.themealdb.com/images/media/meals/vrspxv1511722107.jpg",
+    },
+   
+]
+console.log(dataObj);

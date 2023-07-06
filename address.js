362a8de6bfbdde597e85f7e2ed2bef65b4af3860
unknown <mdamir4298@gmail.com>
2023-07-06 17:51:40 +0530

    payment page some functonality part pending

--- address.js
+++ address.js
@@ -6,9 +6,9 @@ function addressFn(){
     <div id="body1">
      <div class="store_div">
          <p>Address</p>
-         <p><input type="number" placeholder="Pincode" class="input_store"></p>
-         <p><input type="text" placeholder="House/Flate/Office No" class="input_store"></p>
-         <p><input type="text" placeholder="Road Name/ Area/ Colony" class="input_store"></p>
+         <p><input type="number" placeholder="Pincode" class="input_store" required></p>
+         <p><input type="text" placeholder="House/Flate/Office No" class="input_store" required></p>
+         <p><input type="text" placeholder="Road Name/ Area/ Colony" class="input_store" required></p>
  
      </div>
      <div class="store_div">
@@ -16,9 +16,9 @@ function addressFn(){
          
          <p>Information provided here will be used to contact you for delivery updates</p>
         
-         <p><input type="text" placeholder="Full name" class="input_store"></p>
-         <p><input type="number" placeholder="Mobile number" class="input_store"></p>
-         <p><input type="email" placeholder="Email" class="input_store"></p>
+         <p><input type="text" placeholder="Full name" class="input_store" required></p>
+         <p><input type="number" placeholder="Mobile number" class="input_store" required></p>
+         <p><input type="email" placeholder="Email" class="input_store" required></p>
  
      </div>
      <a href="payment.html">
